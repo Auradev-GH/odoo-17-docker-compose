201b2f73f9ddd99509a3c8e3b85a146ae4710a4e
Hamza Benzaoui <Hamzapdd@gmail.com>
2023-11-19 03:29:02 +0100

    update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ DESTINATION=$1
 PORT=$2
 CHAT=$3
 # clone Odoo directory
-git clone --depth=1 https://github.com/minhng92/odoo-16-docker-compose $DESTINATION
+git clone --depth=1 https://github.com/Auradev-GH/odoo-17-docker-compose $DESTINATION
 rm -rf $DESTINATION/.git
 # set permission
 mkdir -p $DESTINATION/postgresql
